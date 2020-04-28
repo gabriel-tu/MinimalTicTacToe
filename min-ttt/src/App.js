474850c7d47030a6e17755f3dab0b0bc44e87b42
Gabriel Tu <gabriel8215@hotmail.com>
2020-04-28 02:59:32 -0400

    added react-bootstrap

--- min-ttt/src/App.js
+++ min-ttt/src/App.js
@@ -1,54 +1,101 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    username: '',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+    };
 
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-//called when the textbox is changed 
-handleChange(event) {
-  this.setState({username: event.target.value});
-}
+  //called when the textbox is changed 
+  handleChange(event) {
+    this.setState({ username: event.target.value });
+  }
 
-//called when the enter button is pressed 
-handleSubmit(event) {
-  alert('A name was submitted: ' + this.state.username);
-  event.preventDefault();
-}
+  //called when the enter button is pressed 
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username);
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Insert username: 
-            <input type="text" value={this.state.username} onChange={this.handleChange}/>
-          </label>
-          <input type = "submit" value="Enter"></input>
-        </form>
+
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Label>Insert username</Form.Label>
+          
+          <Form.Control type="text" value={this.state.username} onChange={this.handleChange} />
+          
+          <Button variant="primary" type="submit" >Enter</Button>
+          
+        </Form>
+
       </div>
     );
   }
 }
 
 
+
+
 class Game extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
-        pass
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
   }
 }
+
+//Renders one square in the tictactoe board 
+function Square(props) {
+  return (
+    <Button className="square" onClick={props.onClick}>
+      {props.value}
+    </Button>
+  );
+}
+
+
+
+
 
 class Chat extends React.Component {
   render() {
